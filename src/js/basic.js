2ba654be436f46fbde0056f809243c8351578d32
Aleksey <alexdm@tv-pilot.ru>
2022-09-21 16:13:54 +0300

    «Unit-тестирование» - Чистые функции

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,12 +1,12 @@
-//export default function getHealth({ name, health }) {
-//const name = "Маг";
-//  if (((name = "Маг"), health > 50)) return "healthy";
-//  if (((name = "Маг"), health >= 15)) return "wounded";
-//  return "critical";
-//}
-
 export default function getHealth(player) {
-  const status = { name: "Маг", healthy: 51, wounded: 15, critical: 0 };
-  const name = "Маг";
+  const status = { healthy: 51, wounded: 15, critical: 0 };
   return Object.keys(status).find((name) => status[name] <= player.health);
 }
+
+// const health = getHealth({ name: "Маг", health: 51 });
+
+// function getHealth(player) {
+//   const status = { healthy: 51, wounded: 15, critical: 0 };
+//   return Object.keys(status).find((name) => status[name] <= player.health);
+// }
+// console.log(health)
